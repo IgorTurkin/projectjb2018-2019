2e3d9988b7c050092ce6d6dbfbb08ab6f9071e83
titovtima <titov.tima@mail.ru>
2018-12-23 23:46:13 +0300

    add unuseful files

---
+++ app/src/main/java/com/example/titovtima/mathgame/ChooseTask.kt
@@ -0,0 +1,14 @@
+package com.example.titovtima.mathgame
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ChooseTask : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_choose_task)
+
+//        when (this.intent.getStringExtra(Constants.keysubject))
+    }
+}
